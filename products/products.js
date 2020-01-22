56fbce6df861cb231185181146313cca372cc8a3
Rex Bergeron <rexbergeron@comcast.net>
2020-01-22 09:52:31 -0800

    render working... I should have committed earlier, but I forgot :(

---
+++ products/products.js
@@ -0,0 +1,10 @@
+import islands from '../data/islands.js';
+import renderIsland from './render-island.js'
+
+const list = document.getElementById('islands');
+
+for(let i = 0; i < islands.length; i++) {
+    const island1 = islands[i];
+    const dom = renderIsland(island1);
+    list.appendChild(dom);
+}
