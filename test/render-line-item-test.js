939d2cfac5fb6a01e2569aa4749d980a0e2b8069
Rex Bergeron <rexbergeron@comcast.net>
2020-01-23 10:56:47 -0800

    save css

---
+++ test/render-line-item-test.js
@@ -0,0 +1,12 @@
+import islands from '../data/islands.js';
+import { findById } from '../common/utilities.js';
+import renderLineItem from '../cart/render-line-item.js';
+
+const test = Qunit.test;
+
+Qunit.module('Render Line Item');
+
+test('renders a line item', expect => {
+
+
+})
